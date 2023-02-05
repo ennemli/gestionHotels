132d85eb5a3faf30ded93125e38b70fa4bfb4489
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-02-05 01:32:57 +0100

    added bootstrap and header for react

--- resources/js/frontend/pages/Home.jsx
+++ resources/js/frontend/pages/Home.jsx
@@ -1,11 +1,12 @@
-import Header from "../layouts/header";
+import Header from "../layouts/Header";
 import Section from "../layouts/Section";
 
 export default function(){
     return (
         <>
         <Header/>
-        <Section/>
+        {/* <Section/> */}
+        {/* <Footer/> */}
         </>
     )
 }
