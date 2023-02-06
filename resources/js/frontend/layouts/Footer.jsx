4f77f14d698513fed9a785e63d842e39a170b1a5
Hossam madani <96637186+HossamElmadani@users.noreply.github.com>
2023-02-06 13:07:03 -0800

    I added footer

--- resources/js/frontend/layouts/Footer.jsx
+++ resources/js/frontend/layouts/Footer.jsx
@@ -1,7 +1,224 @@
-import React from 'react'
+import * as React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <div className="div">
+        <div className="div-2">
+          <div className="div-3">
+            <div className="div-4">About us</div>
+            <div className="aaaaaaaaaaaaaaaaaaa-aaaaaaaaaa">
+              aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaa
+            </div>
+          </div>
+          <div className="div-5">
+            <div className="div-6">Contact us</div>
+            <div className="div-7">
+              <div className="div-8">
+                <picture>
+                  <source
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b"
+                    type="image/webp"
+                  />
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b"
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fb75c29c6dcc44665a837964b9400e21b"
+                    className="image"
+                  />
+                </picture>
+                <div className="builder-image-sizer image-sizer" />
+              </div>
+              <div className="div-9">
+                <picture>
+                  <source
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b"
+                    type="image/webp"
+                  />
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b"
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fc0f110c51d434a1d9037a4976d8ff84b"
+                    className="image"
+                  />
+                </picture>
+                <div className="builder-image-sizer image-sizer" />
+              </div>
+              <div className="div-10">
+                <picture>
+                  <source
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31"
+                    type="image/webp"
+                  />
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31"
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6afe508c66124f7399c59c95ca5ade31"
+                    className="image"
+                  />
+                </picture>
+                <div className="builder-image-sizer image-sizer" />
+              </div>
+              <div className="div-11">
+                <picture>
+                  <source
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f"
+                    type="image/webp"
+                  />
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f"
+                    srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F3b56198be3104d2ebb2524e9f2a7347f"
+                    className="image"
+                  />
+                </picture>
+                <div className="builder-image-sizer image-sizer" />
+              </div>
+            </div>
+          </div>
+          <div className="div-12">
+            <div className="div-13">Aide</div>
+            <div className="aaaaaaaaaaaaaaaaaaa-aaaaaaaaaa">
+              aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaa
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .div {
+          display: flex;
+          flex-direction: column;
+          max-width: 100%;
+          justify-content: center;
+          align-items: center;
+        }
+        .div-2 {
+          display: flex;
+          flex-direction: row;
+          max-width: 100%;
+          justify-content: center;
+          align-items: flex-start;
+          padding-top: 50px;
+          padding-right: 246px;
+          padding-bottom: 50px;
+          padding-left: 246px;
+          background-color: rgba(0, 113, 194, 1);
+        }
+        .div-3 {
+          display: flex;
+          flex-direction: column;
+          max-width: 196px;
+          justify-content: center;
+          align-items: center;
+        }
+        .div-4 {
+          max-width: 77px;
+          color: rgba(255, 168, 0, 1);
+          font-size: 20px;
+          letter-spacing: 0%;
+          text-align: center;
+          font-family: "Gudea", sans-serif;
+        }
+        .aaaaaaaaaaaaaaaaaaa-aaaaaaaaaa {
+          max-width: 196px;
+          margin-top: 19px;
+          color: rgba(255, 255, 255, 1);
+          font-size: 20px;
+          letter-spacing: 0%;
+          text-align: center;
+          font-family: "Gudea", sans-serif;
+        }
+        .div-5 {
+          display: flex;
+          flex-direction: column;
+          max-width: 196px;
+          justify-content: center;
+          align-items: center;
+          margin-left: 180px;
+        }
+        .div-6 {
+          max-width: 92px;
+          color: rgba(255, 168, 0, 1);
+          font-size: 20px;
+          letter-spacing: 0%;
+          text-align: center;
+          font-family: "Gudea", sans-serif;
+        }
+        .div-7 {
+          display: flex;
+          flex-direction: row;
+          max-width: 196px;
+          justify-content: center;
+          align-items: center;
+          margin-top: 24px;
+        }
+        .div-8 {
+          display: flex;
+          position: relative;
+          min-width: 20px;
+          min-height: 20px;
+          max-width: 40px;
+          width: 40px;
+        }
+        .image {
+          object-fit: cover;
+          object-position: center;
+          position: absolute;
+          height: 100%;
+          width: 100%;
+          top: 0;
+          left: 0;
+        }
+        .image-sizer {
+          width: 100%;
+          padding-top: 100%;
+          pointer-events: none;
+          font-size: 0;
+        }
+        .div-9 {
+          display: flex;
+          position: relative;
+          min-width: 20px;
+          min-height: 20px;
+          max-width: 40px;
+          width: 40px;
+          margin-left: 12px;
+        }
+        .div-10 {
+          display: flex;
+          position: relative;
+          min-width: 20px;
+          min-height: 20px;
+          max-width: 40px;
+          width: 40px;
+          margin-left: 12px;
+        }
+        .div-11 {
+          display: flex;
+          position: relative;
+          min-width: 20px;
+          min-height: 20px;
+          max-width: 40px;
+          width: 40px;
+          margin-left: 12px;
+        }
+        .div-12 {
+          display: flex;
+          flex-direction: column;
+          max-width: 196px;
+          justify-content: center;
+          align-items: center;
+          margin-left: 180px;
+        }
+        .div-13 {
+          max-width: 39px;
+          color: rgba(255, 168, 0, 1);
+          font-size: 20px;
+          letter-spacing: 0%;
+          text-align: center;
+          font-family: "Gudea", sans-serif;
+        }
+      `}</style>
+    </>
+  );
 }
