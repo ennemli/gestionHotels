eaa24ab0de0438351fec73f029b9bf093200bf64
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-04-14 01:30:29 +0000

    Linked login with auth api

--- resources/js/frontend/routes/LogIn.jsx
+++ resources/js/frontend/routes/LogIn.jsx
@@ -2,31 +2,45 @@ import React from 'react'
 import Header from '../layouts/Header'
 import Navbar  from '../components/home/Navbar'
 import Section from '../layouts/Section'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
 export default function LogIn() {
-  
+  const navigate=useNavigate()
+  const SubmitHandler=(e)=>{
+    const {target:form}=e
+    const {email,password}=form
+    e.preventDefault()
+    axios.post('http://127.0.0.1:8000/api/login',{
+        email:email.value,
+        password:password.value
+    }).then((res)=>{
+      const {data}=res
+      Cookies.set('access_token',data.access_token,{expires:data.expires_in})
+      navigate('/profile')
+    })
+  }
   return (
     <>
     <Header Navbar={Navbar}/>
 
     <Section>
         
-        <div className='row'>
+        <form  onSubmit={SubmitHandler} className='row'>
 
             <div className='col-6' style={{margin:"25vh auto",padding:"1.2rem",boxShadow:"0 0 6px 2px #eeeeee, 0 0 5px 1px #0171c345"}}>
                     <div className="form-floating w-100 mb-2">
-                        <input type="text" className="form-control " id="floatingInputGroup1" placeholder="Email" />
+                        <input type="text" className="form-control " id="floatingInputGroup1" placeholder="Email" name='email'/>
                         <label htmlFor="floatingInputGroup1">Email</label>
                     </div>
                     <div className="form-floating w-100 mb-2">
-                        <input type="text" className="form-control " id="floatingInputGroup1" placeholder="Password" />
+                        <input type="text" className="form-control " id="floatingInputGroup1" placeholder="Password" name='password'/>
                         <label htmlFor="floatingInputGroup1">Password</label>
                     </div>
                     
-                    <button type="button" className='btn btn-outline-primary '>Log In</button>
+                    <button type="submit" className='btn btn-outline-primary '>Log In</button>
                     <NavLink to={'/signup'} className="btn btn-outline-warning mx-2">Sign Up</NavLink>
                     </div>
-                    </div>
+                    </form>
     </Section>
     </>
   )
